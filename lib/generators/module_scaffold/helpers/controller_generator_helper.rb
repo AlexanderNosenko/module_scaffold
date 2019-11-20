784aa19f7bbdc62d1ddae9b148396b9fa377be8c
Alexander Nosenko <sania.ace@gmail.com>
2019-11-20 02:23:29 +0200

    Fix controller bugs

--- lib/generators/module_scaffold/helpers/controller_generator_helper.rb
+++ lib/generators/module_scaffold/helpers/controller_generator_helper.rb
@@ -8,10 +8,6 @@ class ControllerGeneratorHelper
     'controller.erb'
   end
 
-  def class_name
-    "#{module_name.pluralize}#{helper_type}"
-  end
-
   def class_file_name(_)
     "#{resource_name_plural}_#{helper_type.underscore}.rb"
   end
@@ -24,11 +20,19 @@ class ControllerGeneratorHelper
     options[:'routes-namespace']
   end
 
+  def class_name
+    "#{module_name.pluralize}#{helper_type}"
+  end
+
   def actions
     @default_actions ||= [:index, :show, :create, :update, :destroy]
     return @default_actions if options.blank?
 
     options[:'controller-actions'].map(&:to_sym).presence || @default_actions
+  end
+
+  def services_helper
+    @services_helper ||= ServicesGeneratorHelper.new(module_full_name, options)
   end
 
   private
