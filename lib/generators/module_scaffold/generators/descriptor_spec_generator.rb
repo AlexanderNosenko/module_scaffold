167e8b892bc9ca19514a2a4027a52c45f9c03e88
Alexander <sania.ace@gmail.com>
2019-11-20 17:33:27 +0200

    Model relationships descriptors generation implemented (#11)

--- lib/generators/module_scaffold/generators/descriptor_spec_generator.rb
+++ lib/generators/module_scaffold/generators/descriptor_spec_generator.rb
@@ -8,12 +8,24 @@ class DescriptorSpecGenerator
     'specs/descriptor.erb'
   end
 
+  def class_file_name(_)
+    "#{resource_name}_#{helper_type.underscore}.rb"
+  end
+
   def class_name
     "#{module_name}#{helper_type}"
   end
 
-  def class_file_name(_)
-    "#{resource_name}_#{helper_type.underscore}.rb"
+  def full_class_name
+    "#{module_full_name}#{helper_type}"
+  end
+
+  def generate_files(template_processor:, **)
+    generate_file(self, template_processor)
+
+    model_relationships_descriptor_generators.each do |generator|
+      generate_file(generator, template_processor)
+    end
   end
 
   def format_attr_str(attr)
@@ -24,7 +36,25 @@ class DescriptorSpecGenerator
     policy_generator.permitted_attributes
   end
 
+  def model_relationships_descriptor_generators
+    model_relationships.map do |relationship|
+      self.class.new(relationship.klass.name, options)
+    end
+  end
+
   private
+
+  def model_relationships
+    model_class.reflect_on_all_associations
+  end
+
+  def generate_file(generator, template_processor)
+    template_processor.call(
+      generator,
+      generator.template_path(nil),
+      generator.class_file_path(nil)
+    )
+  end
 
   def helper_type
     'Descriptor'
